0cbc7c7d331e8d77864748cc272e0ef003f44651
ゲッツ <>
2019-06-02 00:01:07 +0900

    表示項目を追加

--- sample/script.js
+++ sample/script.js
@@ -1,3 +1,13 @@
 let dom = document.querySelector('#root');
-let element = React.createElement('p', {}, 'Hello React! Application');
+let element = React.createElement(
+    'div', {}
+        , [ React.createElement('h2', {}, "Hello!")
+        , React.createElement('h3', {}, "React sample page.")
+        , React.createElement('ul', {}, [
+            React.createElement('li', {}, "First Item.")
+            , React.createElement('li', {}, "Second Item.")
+            , React.createElement('li', {}, "Third Item.") 
+            , 
+        ]),
+        ]);
 ReactDOM.render(element, dom);
